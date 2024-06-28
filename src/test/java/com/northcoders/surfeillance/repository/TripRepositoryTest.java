8f93f2055db5a235b02f017e198a72fcc18ce09d
ste <mrbarrow@hotmail.co.uk>
2024-06-28 15:15:57 +0100

    TripRepo tested with static data

--- src/test/java/com/northcoders/surfeillance/repository/TripRepositoryTest.java
+++ src/test/java/com/northcoders/surfeillance/repository/TripRepositoryTest.java
@@ -1,17 +1,24 @@
 package com.northcoders.surfeillance.repository;
 
+import com.northcoders.surfeillance.model.AppUser;
+import com.northcoders.surfeillance.model.SkillLevel;
+import com.northcoders.surfeillance.model.Spot;
 import com.northcoders.surfeillance.model.Trip;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
@@ -20,9 +27,10 @@ class TripRepositoryTest {
     @Autowired
     private TripRepository tripRepository;
 
+
+
     @BeforeEach
     void setup() {
-
     }
 
     // The tests in this file are based on the data.sql file as of 28/06/2024 14:36pm
