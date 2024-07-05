0251b6314ee82fceda66b750aa3fbb876fd89fb9
ste <mrbarrow@hotmail.co.uk>
2024-07-05 14:44:14 +0100

    fix: ConditionsDTO reshaped

--- src/main/java/com/northcoders/surfeillance/service/apis/waveAPI/CurrentWaveData.java
+++ src/main/java/com/northcoders/surfeillance/service/apis/waveAPI/CurrentWaveData.java
@@ -2,6 +2,6 @@ package com.northcoders.surfeillance.service.apis.waveAPI;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record CurrentWaveData(String time, Long interval, Long wave_height, Long wave_direction, Long wave_period){
+public record CurrentWaveData(String time, Long interval, Double wave_height, String wave_direction, Double wave_period){
 }
 
